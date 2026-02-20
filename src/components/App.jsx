c7d218c882027345f0490eba43f1f41d7be2c78a
Gloria R <norimaqidoe@hotmail.com>
2026-02-19 20:16:39 -0600

    feat: add registration logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,32 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import MyProfile from "./MyProfile";
 import "./styles/App.css";
 import { useState } from "react";
 import ProtectedRoute from "./ProtectedRoute";
 import Login from "./Login";
+import * as auth from "../utils/auth";
+import Register from "./Register";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password === confirmPassword) {
+      auth
+        .register(username, password, email)
+        .then(() => {
+          navigate("/login");
+        })
+        .catch(console.error);
+    }
+  };
 
   return (
     <Routes>
@@ -36,6 +55,15 @@ function App() {
         element={
           <div className="loginContainer">
             <Login />
+          </div>
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          <div className="registerContainer">
+            <Register handleRegistration={handleRegistration} />
           </div>
         }
       />
